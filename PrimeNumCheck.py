9afc013d2bdb10b80295d49e108863fa63194861
Lixx1423 <70164180+Lixx1423@users.noreply.github.com>
2021-08-27 09:23:43 -0400

    Update PrimeNumCheck.py

--- PrimeNumCheck.py
+++ PrimeNumCheck.py
@@ -1,15 +1,19 @@
 
-num = int(input("Give me a number you think is prime: "))
+def primenum():
 
-if num > 1:
+    num = int(input("Give me a number you think is prime: "))
 
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"Is not a prime number")
-           print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"Is indeed a prime number")
-       
-else:
-   print(num,"Will never be prime")
+    if num > 1:
+
+        for i in range(2,num):
+            if (num % i) == 0:
+                print(num,"Is not a prime number")
+                print(i,"times",num//i,"is",num)
+                primenum()
+                break
+        else:
+            print(num,"Is indeed a prime number")
+            primenum()
+    else:
+        print(num,"Will never be prime")
+primenum()
